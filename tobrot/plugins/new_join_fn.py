99b0f8a894137d9711a588834407607d5888bd92
MR. RANGO <74708667+RangoZEX@users.noreply.github.com>
2022-01-14 11:21:51 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -102,38 +102,3 @@ __Send any one of the available command, as a reply to a valid link/magnet/torre
     )
 
 
-################## Start Command ##############
-@Client.on_message(filters.command("start") & filters.private & filters.incoming)
-async def start(c, m):
-    await m.reply_text(
-        text=HOME_TEXT.format(m.from_user.first_name),
-        disable_web_page_preview=True,
-        parse_mode="markdown",
-        quote=True,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton('💡 Help', callback_data='help'),
-                    InlineKeyboardButton('📝 About', callback_data='about')
-                ],
-                [
-                    InlineKeyboardButton('Close', callback_data='close')
-                ]
-            ]
-        )
-    )
-
-
-
-############################# HOME TEXT #####################
- 
-HOME_TEXT = """Hi.. 🙋
- 
-💡 **I am a Telegram Leech Bot**
- 
-🏷️ __I can Leech through torrent, Direct, Ytdl Link and upload to telegram 😌__.
- 
- **For check my Features and all commands:**
-👉 /help
- 
-👲 **Maintained By**: [👷 MAXX](https://t.me/MaxxRiderz)"""
